eb9ce1081c3f50e3a2b691ac649e05102dc46d79
tohidshabanloo <tshabanloo@hotmail.com>
2022-04-29 13:50:09 +0430

    some change

--- src/components/Button/Button.js
+++ src/components/Button/Button.js
@@ -2,15 +2,7 @@ import React from "react";
 import "./Button.css";
 
 const Button = (props) => {
-  console.log(props);
-  return (
-    <button
-      style={{ color: props.color, ...props.style }}
-      className={props.className}
-    >
-      {props.children}
-    </button>
-  );
+  return <button {...props}>{props.children}</button>;
 };
 
 export default Button;
--- src/components/Content/Content.js
+++ src/components/Content/Content.js
@@ -1,19 +1,36 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Content.css";
 import Button from "../Button/Button";
 
 const Content = () => {
+  const btn = ["Google Search", "I'm Feeling Lucky"];
+  const [val, func] = useState(2);
+  const addNew = () => {
+    func(val ** 2);
+  };
+  const remove = () => {
+    func(val - 1);
+  };
   return (
     <div className="container">
       <h3>Google</h3>
+
       <input className="input"></input>
       <div>
-        <Button color="white" className="bgRed">
-          Google Search
-        </Button>
-        <Button color="white" className="bgRed">
-          I'm Feeling Lucky
-        </Button>
+        <button onClick={addNew}>ADD</button>
+        <div>{val}</div>
+        <button onClick={remove}>Delete</button>
+      </div>
+      <div>
+        {btn.map((el) => {
+          return (
+            <>
+              <Button color="white" className="bgRed">
+                {el}
+              </Button>
+            </>
+          );
+        })}
       </div>
     </div>
   );
